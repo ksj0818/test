a4d568123552943d7f79bad528dca41e012f73ce
SungJoonKim <88412911+ksj0818@users.noreply.github.com>
2021-12-21 15:06:48 +0900

    HTML 모듈화 하는 방법

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# test
+# HTML 모듈화
+
+- includeHTML.js 파일 생성
+
+- index.html head안에 includeHTML.js, style.css import하기
+
+- header.html, nav.html, footer.html 각 파일 생성
+
+- body안에 <div data-include-path="header.html"></div> 태그 삽입
---
+++ js/includeHTML.js
@@ -0,0 +1,17 @@
+// 페이지 로딩 시 발생하는 이벤트
+window.addEventListener("load", function () {
+  var allElements = document.getElementsByTagName("*");
+  Array.prototype.forEach.call(allElements, function (el) {
+    var includePath = el.dataset.includePath;
+    if (includePath) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          el.outerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", includePath, true);
+      xhttp.send();
+    }
+  });
+});
